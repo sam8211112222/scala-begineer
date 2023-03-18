fc02ad8dd4e9831ca1d9ab5606777f0d02b7f2c7
Sam Chen <sam8211112222@gmail.com>
2023-03-18 12:18:38 +0800

    Values,Variables and Types

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,27 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes {
+
+  // vals are immutable
+  val x :Int = 42;
+  println(x)
+
+  // compiler can infer types
+  val y ="Hi";
+  println(y)
+
+  // scala types as following
+  val aInt:Int =1;
+  val aBoolean:Boolean =true;
+  val aChar:Char ='a';
+  val aShort:Short =12;
+  val aFloat:Float =1.1f;
+  val aLong:Long =1L;
+  val aDouble:Double =1.0;
+
+  // variables
+  var z :Int = 1;
+  z = z+1;
+  println(z);
+
+}
