9d87ed40b23a3ab5be0a2d69d1198b6868dfe078
Sam Chen <sam8211112222@gmail.com>
2023-06-26 10:51:24 +0800

    Recursion practice

--- src/main/scala/lectures/part1basics/Recursion.scala
+++ src/main/scala/lectures/part1basics/Recursion.scala
@@ -25,4 +25,44 @@ object Recursion extends App {
   }
 
   println(anotherFactorial(10000))
+
+
+  // task
+  //1.Contatenate a string n times
+  @tailrec
+  def stringConcatenate(stringNeedToConcat: String, times:Int,accumulator:String):String={
+      if (times < 1) accumulator
+      else stringConcatenate(stringNeedToConcat,times-1,accumulator+stringNeedToConcat)
+  }
+  println(  stringConcatenate("Test",3,""))
+  //2.IsPrime function tail recursive
+
+  def isPrime(n:Int):Boolean={
+    @tailrec
+    def isPrimeTailrec(t:Int, isStillPrime:Boolean):Boolean= {
+      if (!isStillPrime) false
+      else if (t <= 1) true
+      else isPrimeTailrec(t - 1, n % t != 0 && isStillPrime)
+    }
+      isPrimeTailrec(n/2,true)
+  }
+  println(isPrime(2003))
+  println(isPrime(629))
+
+  //3.Fibonacci function, tail recursive
+
+  def fib(n:Int):Int={
+
+    @tailrec
+    def fibHelper(i: Int, last: Int, nextToLast: Int): Int = {
+      if (i>= n)last
+      else
+        fibHelper(i+1, last+nextToLast, last)
+    }
+
+    if (n<=2)1
+    else fibHelper(2, 1, 1)
+  }
+  println(fib(5))
+
 }
